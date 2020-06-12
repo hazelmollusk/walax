5e5f54b1c8389c903a3cf493163604b863452a99
Matt Barry <matt@hazelmollusk.org>
2020-06-12 12:09:12 -0400

    organize some things, set up webpack configuration

---
+++ walax-client/src/Walax.js
@@ -0,0 +1,15 @@
+const { observable } = require('mobx')
+
+class Walax extends Object {
+  static #_instances = observable.map() 
+
+  static get instance (key = false) {
+    key = key || -47
+    if (!this.#_instances.has(key)) 
+      this.#_instances.set(key, observable.box(new this(key)))
+    
+    return this.#_instances.get(key)
+  }
+}
+
+export default Walax
---
+++ walax-client/src/index.js
@@ -0,0 +1,7 @@
+import Control from './control/Control'
+import ModelBase from './model/ModelBase'
+import Model from './model/Model'
+import Manager from './model/Manager'
+import Walax from './Walax.js'
+
+export default { Walax, Control, ModelBase, Model, Manager }
