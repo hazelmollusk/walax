bfcfb2964f09411a5abe92ee84381a438f93f181
Matt Barry <matt@hazelmollusk.org>
2020-06-12 11:29:06 -0400

    initial js commit

---
+++ js/src/Control.js
@@ -0,0 +1,10 @@
+import Walax from './Walax'
+const { observable } = require('mobx')
+
+class Control extends Walax {
+  constructor () {
+    super()
+  }
+}
+
+export default { Control: observable(Control.instance()) }
