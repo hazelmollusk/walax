39691bfab65c0172cae1b1179382b2fcb54cc56e
Matt Barry <matt@hazelmollusk.org>
2020-06-25 08:21:04 -0400

    model/manager stuff that should be in the next branch

--- walax/src/control/ObjectControl.js
+++ walax/src/control/ObjectControl.js
@@ -1,57 +1,49 @@
 import Control from './Control'
 import ModelBase from '../model/ModelBase'
 import w from '../Walax'
+import { observable } from 'mobx'
 
-class ObjectControl extends Control {
-  @observable schema = null
-  
-  /**
-   *Creates an instance of ObjectControl.
-   * @param {boolean} [key=false]
-   * @memberof ObjectControl
-   *
-   * ObjectControl(key) uses the value passed
-   * as a URI for the base schema to load.
-   */
-  constructor (key = false) {
+//todo schema versioning/collision detection/etc
+
+class WalaxSchema {
+
+}
+
+class OpenApiSchema extends WalaxSchema {
+  constructor(uri = false) {
     super()
     this._init()
+    this.uri = uri
   }
-
-  _init () {
-    this.schema = null
+  _init() {
+    this.uri = false
+    this.schema = false
     this._ops = null
     this._models = null
     this._modelNames = null
     this._managers = null
-    this._status = null
   }
-
-  loadSchema (uri, name = false, merge = true) {
-    this._status = false
-    w.log.info(`loading model schema: ${this.schemaUri}`)
-    return w.net.get(this.schemaUri).then(data => this._addSchema(uri, data, name, merge))
+  get uri() { return this._uri }
+  set uri(uri) {
+    w.net.get(this.schemaUri).then(data => {
+      this._init()
+      this._uri = uri
+      this.schema = data
+      // may as well initialize these
+      this.ops && this.modelNames && this.models
+    })
   }
-  
-  _addSchema(uri, data, name, merge) {
-
-  }
-
   get modelNames () {
     if (!this._modelNames) {
       let mappings = new Map()
-      let names = new Set(
-        Object.values(this.ops)
+      let names = new Set(Object
+          .values(this.ops)
           .map(v => v?.op[1]))
       names.forEach(x => {
         // check for plurals
         ['s','es'].forEach(y => {
-          if (names.has(x + y)) {
-            names.delete(x + y)
-            mappings.set(x + y, x)
-          }
-        })
-      })
+            names.delete(x + y) && mappings.set(x + y, x) 
+        })})
       w.log.info('parsed model names', names, mappings)
       this._modelMap = mappings
       this._modelNames = names
@@ -65,7 +57,7 @@ class ObjectControl extends Control {
 
   get models () {
     if (!this._models && this.modelNames && this.ops) {
-      this._models = {}      
+      this._models = observable.map()
       this._modelNames.forEach(model => {
         class WalaxProxyModel extends ModelBase {
           static _wlx_model = model
@@ -81,7 +73,7 @@ class ObjectControl extends Control {
 
   get ops () {
     if (!this._ops && this.schema?.paths) {
-      this._ops = {}
+      this._ops = observable.map()
       Object.entries(this.schema.paths).map(p => 
         Object.entries(methods).map(m => {
           let opId = m[1].operationId
@@ -98,6 +90,63 @@ class ObjectControl extends Control {
       w.log.info('built operations map', this._ops)
     }
     return this._ops
+  }
+}
+
+
+class ObjectControl extends Control {
+  /**
+   *Creates an instance of ObjectControl.
+   * @param {boolean} [key=false]
+   * @memberof ObjectControl
+   *
+   * ObjectControl(key) uses the value passed
+   * as a URI for the base schema to load.
+   */
+  constructor (key = false) {
+    super()
+    this._init(true)
+  }
+
+  _init (full=false) {
+    if (full)
+      this.schema = observable.map()
+    this._ops = null
+    this._models = null
+    this._modelNames = null
+    this._managers = null
+    this._status = null
+  }
+  
+  loadSchema(schema, name) {
+    // pre-built WalaxSchema
+    // todo checking, implement
+  }
+  
+  loadUri (uri, name = false, replace = false) {
+    w.log.info(`loading model schema: ${uri}`)
+    this._init(replace)
+    this.schema.set(name, new OpenApiSchema(uri))
+  }
+
+  get ops() {
+    if (!this._ops) {
+      this._ops = observable.map()
+      this.schema.forEach(v, k => v.ops.forEach(vv, kk => {
+        this._ops.set(kk, vv) //todo collision detection
+      }))
+    }
+    return this._ops
+  }
+
+  get models() {
+    if (!this._models) {
+      this._models = observable.map()
+      this.schema.forEach(v, k => v.models.forEach(vv, kk => {
+        this._models.set(kk, vv) //todo collision detection
+      }))
+    }
+    return this._models
   }
 }
 
