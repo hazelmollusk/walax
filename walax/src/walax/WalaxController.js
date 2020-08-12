e246475d7151301a06d79d0f318c963c649e883c
Matt Barry <matt@hazelmollusk.org>
2020-08-11 23:00:47 -0400

    split some objects up

---
+++ walax/src/walax/WalaxController.js
@@ -0,0 +1,24 @@
+const { observable } = require('mobx')
+
+export const WalaxController = {
+  all: new Map()
+  keys: new Set()
+
+  isValidProp (key) { return !this.keys.has(key) /* fixme */ },
+
+  register (Klass, key = false, ...args) {
+    if (!this.all.has(Klass)) {
+      const controller = new Klass(...args)
+      this.all.set(Klass, controller))
+      if (this.isValidProp(key)) {
+        this.keys.set(key, all.get(Klass))
+        Object.defineProperty(this, key, {
+          get: function () { return this.keys.get(key) }
+        }) 
+      }
+    }
+    return this.all.get(Klass)
+  }
+}
+
+export default WalaxController
---
+++ walax/src/walax/WalaxLogger.js
@@ -0,0 +1,44 @@
+const FATAL = 0
+const ERROR = 1
+const WARN = 2
+const INFO = 4
+const DEBUG = 8
+
+export const consoleLog = (msg, lvl) => console.log(msg)
+export const recordLogs = (msg, lvl) => true // fixne
+
+export const WalaxLogger = {
+  all = new Set(),
+  level = INFO,
+
+  /**
+   * registers a callback: (msg, level) => { ..logging.. }
+   *
+   * @param {*} cb
+   */
+  register (cb) { this.all.add(cb) },
+
+  /**
+   * public methods for each log level.  accepts arbitrary
+   * argument list, and logs each item individually
+   *
+   * @param {*} s
+   */
+  fatal  (...s) { return (this.level >= FATAL) || this._log(s, INFO) },
+  info   (...s) { return (this.level >= INFO) || this._log(s, INFO) },
+  warn   (...s) { return (this.level >= WARN) || this._log(s, WARN) },
+  error  (...s) { return (this.level >= ERROR) || this._log(s, ERROR) },
+  debug  (...s) { return (this.level >= DEBUG) || this._log(s, DEBUG) },
+
+  async _log (s, level = INFO) {
+    return Array.isArray(s)
+      ? s.map(x => this._log(x, level))
+      : this._processLog(s, level)
+  },
+
+  async _processLog (msg, level) {
+    this.all.forEach((f, k, s) => f(msg, level))
+  }
+}
+
+export default WalaxLogger
