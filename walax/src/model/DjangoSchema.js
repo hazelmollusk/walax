002cb88955f10688ace4fd136af70f27abb536d1
Matt Barry <matt@hazelmollusk.org>
2020-08-14 07:21:33 -0400

    schema class changes

--- walax/src/model/DjangoSchema.js
+++ walax/src/model/DjangoSchema.js
@@ -2,113 +2,36 @@ import WalaxModel from './WalaxModel'
 import { observable } from 'mobx'
 import { WalaxSchema } from './WalaxSchema'
 const m = require('mithril')
-
+const w = require('../Walax.js')
 
 class ApiOperation {
   constructor(path) {
     this.summary = path.summary || ''
-    this.description = path.description || ''
+     this.description = path.description || ''
   }
 
 }
 
 export class DjangoSchema extends WalaxSchema {
   _uri = false
+  _url = false
   _servers = false
+  _customModels = false
   ops = observable.map()
   models = observable.map()
 
   constructor(uri = false, models = false) {
     super()
-    if (uri) this.load(uri, models)
+      this.initialize()
+    if (uri) this.loadURI(uri, models)
   }
 
-  load(uri, models = false) {
-    if (!this.checkUri(uri)) 
-      throw new URIError(`invalid schema URI: ${uri}`)
-    if (!models || !this.checkModels(models)) 
-      throw new TypeError(`invalid models: ${uri}`)
-    w.net.get(uri).then(data => {
-      if (!this.checkSchema(data))
-        throw new TypeError(`invalid schema response: ${uri}`)
-      this.schema = data 
-      this.models.clear()
 
-      // work
-      this.title = data.info.title || 'unnamed'
-      this.version = data.info.version || -1
-      this.description = data.info.description || ''
-      this._servers = data.servers || false   // todo
-      this._uri = uri
-      // Object.entries(data.paths)
-    })
-  }
-  
-  get uri() { return this._uri }
-  set uri(uri) { this.load(uri) }
-  checkUri(uri) {
 
-  }
+  // uri in this case is expected to be a DRF API root URL
 
-  get modelNames() {
-    if (!this._modelNames) {
-      let mappings = new Map()
-      console.log(this.ops)
-      let names = new Set(Object
-        .values(this.ops)
-        .map(v => v?.op[1]))
-      names.forEach(x => {
-        // check for plurals
-        ['s', 'es'].forEach(y => {
-          names.delete(x + y) && mappings.set(x + y, x)
-        })
-      })
-      w.log.info('parsed model names', names, mappings)
-      this._modelMap = mappings
-      this._modelNames = names
-    }
-    return this._modelNames
-  }
-
-  get modelMap() {
-    return this.modelNames && this._modelMap
-  }
-
-  get models() {
-    if (!this._models && this.modelNames && this.ops) {
-      this._models = observable.map()
-      this._modelNames.forEach(model => {
-        class WalaxProxyModel extends WalaxModel {
-          static _wlx_model = model
-        }
-        // todo add properties 
-        this._models[model] = WalaxProxyModel
-
-      })
-      w.log.info('regenerated model classes', this._models)
-    }
-    return this._models
-  }
-
-  get ops() {
-    if (!this._ops && this.schema?.paths) {
-      this._ops = observable.map()
-      Object.entries(this.schema.paths).map(paths => 
-            Object.entries(paths[1]).map(path => {
-        let opId = path[1].operationId
-        
-        // this is how we store model definitions internally
-        this._ops[opId] = {
-          path: paths[0],
-          method: path[0],
-          detail: path[1],
-          op: opId
-            .match(/[A-Z]?[a-z]+/g)
-            .map(s => s.toLowerCase())
-        }
-      }))
-      w.log.info('built operations map', this._ops)
-    }
-    return this._ops
+  parseData(data, models) {
+    this.parseInfo(data)
+    
   }
 }
--- walax/src/model/WalaxSchema.js
+++ walax/src/model/WalaxSchema.js
@@ -3,14 +3,60 @@ import WalaxModel from './WalaxModel'
 //todo schema versioning/collision detection/etc
 export class WalaxSchema {
     schema = false
-    models = false
-    ops = false
     title = false
     description = false
     version = false
-
+    _uri = false
+    _url = false
+    _servers = false
+    _customModels = false
+    models = observable.map()
+  initialize() {
+    this._uri = false
+    this._url = false
+    this._servers = falseeE
+    this._customModels = false
+    ops.clear()
+    models.clear()
+  }
     checkModel(model) {
         if (!(model instanceof WalaxModel)) return false
         return true
+    }
+
+    get uri() { return this._uri }
+    set uri(uri) { this.loadURI(uri) }
+
+    loadURI(uri, models = false) {
+
+        this.initialize()
+        this._url = new URI(uri)  // this will throw a TypeError if invalid
+        this._uri = uri
+
+        
+        if (!models || !this.parseModels(models)) 
+            throw new TypeError(`invalid models: ${uri}`)
+        
+        w.net.get(uri).then(data => this.parseData(data, models))
+    }
+
+    parseModels(models) {
+            if (!models) return true
+        models.forEach( (v, k) => {
+            if (!w.isValidProp(k))
+                throw new TypeError(`invalid name for model ${k}`)
+            if (!this.checkModel(v))
+                throw new TypeError(`custom model ${k} is not a WalaxObject`)
+        })
+        this._customModels = models
+        return true
+    }
+
+    getModelClass(name) {
+        return WalaxModel
+    }
+
+    parseData(data) {
+        throw new Error('schema class must implement parseData(data)')
     }
 }
