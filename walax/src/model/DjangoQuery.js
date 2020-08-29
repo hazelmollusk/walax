33942ec5f3d6e247b187becd6ad9a13c24ca5b4c
Matt Barry <matt@hazelmollusk.org>
2020-08-29 08:45:41 -0400

    docstring

--- walax/src/model/DjangoQuery.js
+++ walax/src/model/DjangoQuery.js
@@ -1,34 +1,37 @@
-
-
 class DjangoQuery {
   _model = false
   _parent = false
   _flip = false
-
-  constructor(model, parent=false, flip=false, ...args) {
+  /**
+   * Creates an instance of DjangoQuery.
+   * @param {*} model
+   * @param {*} parent
+   * @param {dict} [args=false]
+   * @param {boolean} [flip=false]
+   * @memberof DjangoQuery
+   */
+  constructor (model, parent, args = false, flip = false) {
     this._model = model
     this._parent = parent
     this._flip = flip
     if (args) this.process(args)
   }
 
-  process(args) {
-    
-  }
+  process (args) {}
 
-  all() {
+  all () {
     return new DjangoQuery(this.model, this)
   }
 
-  none() {
-    return new DjangoQuery(this.model, this, true)
+  none () {
+    return new DjangoQuery(this.model, this, false, true)
   }
 
-  filter(...args) {
-    return new DjangoQuery(this.model, this, ...args)
+  filter (args) {
+    return new DjangoQuery(this.model, this, args)
   }
 
-  exclude(...args) {
-    return new DjangoQuery(this.model, this, true, ...args)
+  exclude (args) {
+    return new DjangoQuery(this.model, this, args, true)
   }
 }
