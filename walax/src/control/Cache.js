55b9da49c6efd4375dd799cd34f010723f3330fb
Matt Barry <matt@hazelmollusk.org>
2020-09-05 07:46:14 -0400

    cache interface

--- walax/src/control/Cache.js
+++ walax/src/control/Cache.js
@@ -2,40 +2,41 @@ import { observable } from 'mobx'
 import w from '../Walax'
 
 class WalaxCache {
+  _name = false
   _storage = false
-  constructor () {
+  constructor (name = 'root') {
+    w.dbg(`CACHE NEW: ${name}`)
+    this._name = name
     this._storage = new Map()
   }
   cache (key) {
-    if (!this._storage.has(key)) this._storage.set(key, new WalaxCache())
+    if (!this._storage.has(key)) this._storage.set(key, new WalaxCache(key))
     return this._storage.get(key)
   }
-  find (func, ...args) {
-    console.log('FIND', func, args)
-    if (args.length) {
-      let testVal = args.pop()
-      if (testVal !== undefined) args.push(testVal)
-      let key = args.shift()
-      if (args.length) return this.cache(key).find(func, ...args)
-      if (!this._storage.has(key)) {
-        if (typeof func == 'function') this._storage.set(key, func(key))
-        else if (func === undefined) this._storage.delete(key)
-        else this._storage.set(key, func)
-        console.log('STOR', this._storage, key, func)
-      }
-      return this._storage.get(key)
+  find (key, func, ...args) {
+    w.dbg(`CACHE: ${this._name}.${key}`, func, ...args)
+    if (args.length) return this.cache(args.shift()).find(key, func, ...args)
+    if (!this._storage.has(key)) {
+      w.dbg(`CACHE MISS: ${this._name}.${key} (${typeof func})`)
+      if (typeof func == 'function') this._storage.set(key, func(key))
+      else if (func === undefined) this._storage.delete(key)
+      else this._storage.set(key, func)
+      w.dbg('CACHE STORE: ', this._storage.get(key))
+    } else {
+      w.dbg(`CACHE HIT: ${this._name}.${key}`)
     }
-    return undefined
+    return this._storage.get(key)
   }
-  remove (...args) {
+  remove (key, ...args) {
     let c = this
-    while (args.length > 1) c = c.cache(args.pop())
-    c._storage.delete(args.pop())
-    return true
+    while (args.length) c = c.cache(args.pop())
+    w.dbg(`CACHE DEL: ${this._name}.${key}`)
+    return c._storage.delete(key)
   }
-  store (func, ...args) {
-    this.remove(...args)
-    return this.find(func, ...args)
+  store (key, func, ...args) {
+    w.dbg(`CACHE PUT: ${this._name}.${key}`)
+    this.remove(key, ...args)
+    return this.find(key, func, ...args)
   }
 }
 
