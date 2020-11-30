1c5e433882e8867dc9855d990412a8df5bb04921
Matt Barry <matt@hazelmollusk.org>
2020-11-30 16:56:52 -0500

    add some test data

--- test/records/models.py
+++ test/records/models.py
@@ -13,6 +13,7 @@ GENRE_CHOICES = [
 class Band(models.Model):
     name = models.CharField(max_length=50)
     genre = models.PositiveSmallIntegerField(choices=GENRE_CHOICES)
+    def __str__(self): return self.name
 
 
 class Album(models.Model):
@@ -20,23 +21,24 @@ class Album(models.Model):
     publish_date = models.DateField(auto_now=True)
     genre = models.PositiveSmallIntegerField(choices=GENRE_CHOICES)
     band = models.ForeignKey(Band, on_delete=models.CASCADE)
-
+    def __str__(self): return self.name
 
 class Song(models.Model):
     name = models.CharField(max_length=50)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     genre = models.PositiveSmallIntegerField(choices=GENRE_CHOICES)
-
+    def __str__(self): return self.name
 
 class Store(models.Model):
     name = models.CharField(max_length=50)
     albums = models.ManyToManyField(Album, through='Inventory')
-
+    def __str__(self): return self.name
 
 class Inventory(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     count = models.PositiveSmallIntegerField(default=0)
+    
     class Meta:
         verbose_name_plural = 'Inventories'
