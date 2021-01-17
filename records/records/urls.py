8887dbb6b3b9e2d57bb9257a0d12b30a3013b596
Matt Barry <matt@hazelmollusk.org>
2021-01-17 07:17:33 -0500

    walax model files

--- records/records/urls.py
+++ records/records/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.templatetags.static import static
-from rest_framework import routers, serializers, viewsets
 from rest_framework.schemas import get_schema_view
+from rest_framework import routers, serializers, viewsets
 from .views import *
 
 router = routers.DefaultRouter()
---
+++ src/js/walax/model/django/WalaxManager.js
@@ -0,0 +1,28 @@
+import Manager from '../Manager'
+import DjangoQuery from './DjangoQuery'
+
+export default class DjangoManager extends Manager {
+    constructor(model) {
+        super(model)
+    }
+
+    all() {
+        return new DjangoQuery(this)
+    }
+
+    filter(args) {
+        return this.all().filter(args)
+    }
+
+    exclude(...args) {
+        return this.all().exclude(args)
+    }
+
+    none() {
+        return this.all().none()
+    }
+
+    get serialized() {
+        return `{${this._model._name}}`
+    }
+}
