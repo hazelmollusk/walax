b1683b9aa8b95726e7a135c8b7f457a2503aa60a
Matt Barry <matt@hazelmollusk.org>
2021-06-09 09:05:52 -0400

    records app

--- records/records/static/records/app.js
+++ records/records/static/records/app.js
@@ -1,7 +1,7 @@
-// import w from 'walax'
+import w from 'walax'
 import m from 'mithril'
 import {observable} from 'mobx'
 let url = '/records/api/?format=json'
 let prop = 'records'
 
-console.log(m)
+console.log(w)
--- records/records/static/records/webpack.config.js
+++ records/records/static/records/webpack.config.js
@@ -19,35 +19,35 @@ module.exports = {
     //         'process.env': 'process/browser'
     //     }
     // },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/, // include .js files
-                exclude: /node_modules/, // exclu2234de any and all files in the `node_modules folder`
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env'],
-                            plugins: [
-                                ['@babel/plugin-transform-runtime'],
-                                ['@babel/plugin-syntax-dynamic-import'],
-                                ['@babel/plugin-proposal-class-properties', { loose: true }],
-                                ['@babel/plugin-proposal-object-rest-spread'],
-                                ['@babel/plugin-proposal-logical-assignment-operators'],
-                                ['@babel/plugin-proposal-optional-chaining'],
-                                ['@babel/plugin-proposal-decorators', { legacy: true }],
-                                ['@babel/plugin-transform-react-jsx', {
-                                    "pragma": "m",
-                                    "pragmaFrag": "'['"
-                                }]
-                            ]
-                        }
-                    }
-                ]
-            }
-        ]
-    }
+    // // module: {
+    // //     rules: [
+    // //         {
+    // //             test: /\.(js|jsx)$/, // include .js files
+    // //             exclude: /node_modules/, // exclu2234de any and all files in the `node_modules folder`
+    // //             use: [
+    // //                 {
+    // //                     loader: 'babel-loader',
+    // //                     options: {
+    // //                         presets: ['@babel/preset-env'],
+    // //                         plugins: [
+    // //                             ['@babel/plugin-transform-runtime'],
+    // //                             ['@babel/plugin-syntax-dynamic-import'],
+    // //                             ['@babel/plugin-proposal-class-properties', { loose: true }],
+    // //                             ['@babel/plugin-proposal-object-rest-spread'],
+    // //                             ['@babel/plugin-proposal-logical-assignment-operators'],
+    // //                             ['@babel/plugin-proposal-optional-chaining'],
+    // //                             ['@babel/plugin-proposal-decorators', { legacy: true }],
+    // //                             ['@babel/plugin-transform-react-jsx', {
+    // //                                 "pragma": "m",
+    // //                                 "pragmaFrag": "'['"
+    // //                             }]
+    // //                         ]
+    // //                     }
+    // //                 }
+    // //             ]
+    // //         }
+    // //     ]
+    // }
     // ,
     // optimization: {
     //     //   minimize: false,
